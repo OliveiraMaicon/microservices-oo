1c1f183c5fb9efa6eb4fbd270d05464df4b1e1bb
Maicon Oliveira <maiconoliveira@MacBook-Pro-de-Maicon-Oliveira.local>
2016-08-28 17:39:08 -0300

    add app client

--- oo-service/src/main/java/com/br/oor/Application.java
+++ oo-service/src/main/java/com/br/oor/Application.java
@@ -2,10 +2,12 @@ package com.br.oor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
